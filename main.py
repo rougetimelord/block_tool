36c7b7c969e8aeb34e343c350456842e92a1cf23
Rouge <code@r0uge.org>
2020-10-23 17:33:16 -0700

    Format

--- main.py
+++ main.py
@@ -1,20 +1,14 @@
 import tweepy
 import json
 
+
 def connect(name):
-    auth = tweepy.OAuthHandler(
-        key["con_t"],
-        key["con_s"],
-        "https://auth.r0uge.org"
-    )
-    
-    if (
-        name in key["account_keys"]
-    ):
+    auth = tweepy.OAuthHandler(key["con_t"], key["con_s"], "https://auth.r0uge.org")
+
+    if name in key["account_keys"]:
         print("got cached key for %s" % name)
         auth.set_access_token(
-            key["account_keys"][name]["acc_t"],
-            key["account_keys"][name]["acc_ts"]
+            key["account_keys"][name]["acc_t"], key["account_keys"][name]["acc_ts"]
         )
     else:
         print("setting up key for %s" % name)
@@ -32,11 +26,13 @@ def connect(name):
             exit()
         key["account_keys"][name] = {
             "acc_ts": auth.access_token_secret,
-            "acc_t": auth.access_token}
+            "acc_t": auth.access_token,
+        }
         with open("key.json", "w") as f:
-                json.dump(key, f, indent=4, sort_keys=True)
+            json.dump(key, f, indent=4, sort_keys=True)
 
     return tweepy.API(auth)
+
 
 try:
     with open("key.json", "r") as f:
@@ -54,15 +50,15 @@ block_list = []
 while cursor != 0:
     block_list_resp = export_api.blocks(cursor)
     for user in block_list_resp[0]:
-        block_list.append({
-            'name': user.screen_name,
-            'id': user.id})
+        block_list.append({"name": user.screen_name, "id": user.id})
     cursor = block_list_resp[1][1]
 
 print("blocking %i accounts on %s" % (len(block_list), import_name))
 for index, acct in enumerate(block_list):
     try:
-        print("%04i blocking %s" % (index + 1, acct['name']))
-        import_api.create_block(user_id=acct['id'], skip_status=True, include_entities=False)
+        print("%04i blocking %s" % (index + 1, acct["name"]))
+        import_api.create_block(
+            user_id=acct["id"], skip_status=True, include_entities=False
+        )
     except tweepy.TweepError as e:
         print(e)
